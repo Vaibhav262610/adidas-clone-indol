62039fb28b7c3bf1f83864c77eea738ae9a316d2
Vaibhav262610 <branda56235@gmail.com>
2022-07-01 11:25:34 +0530

    add

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <div className="w-9/12  text-white">
           <h1 className="uppercase text-6xl font-black">LIGHT UP THE PITCH</h1>
           <h1 className="font-semibold text-sm mt-2">
-            Best of Adidas&#44; IT'S YOUR PLAY&#44; START TODAY
+            Best of Adidas&#44; IT&apos;S YOUR PLAY&#44; START TODAY
           </h1>
           <button className="bg-white items-center font-bold mt-6 flex gap-2 text-black uppercase text-sm px-4 py-4 ">
             Shop Now
